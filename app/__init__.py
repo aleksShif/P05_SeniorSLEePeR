802a97599a9118fd53ab27dddc9b4e3ac4697682
Ryan Lau <47727459+ryanlau@users.noreply.github.com>
2023-06-03 10:39:40 -0400

    login, register, scrollbar

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,14 @@ app = Flask(__name__)
 def root():
     return render_template("landing.html")
 
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
 if __name__ == "__main__":
     app.debug = True
     app.run()
