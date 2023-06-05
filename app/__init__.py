bd10bbd687f7080a55b760b38b909cd37d16a461
Ryan Lau <47727459+ryanlau@users.noreply.github.com>
2023-06-05 11:22:16 -0400

    ?

--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,8 @@ app.secret_key = b'pAHy827suhda*216jdaa'
 
 @app.route("/")
 def root():
+    if 'username' in session:
+        return redirect(url_for("catalog"))
     return render_template("landing.html")
 
 @app.route("/login", methods=['GET', 'POST'])
@@ -90,7 +92,7 @@ def page_not_found(e):
 
 @app.errorhandler(500)
 def page_not_found(e):
-    return render_template('error.html', error_code=500, error_message="Uh oh, something went wrong."), 404
+    return render_template('error.html', error_code=500, error_message="Uh oh, something went wrong."), 500
 
 
 if __name__ == "__main__":
