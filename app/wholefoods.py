5f86302b12ad07bfe335c298b8c3b383430be69c
Lauren <llee30@stuy.edu>
2023-06-06 13:53:04 -0400

    get all from wholefoods

--- app/wholefoods.py
+++ app/wholefoods.py
@@ -3,19 +3,35 @@ import requests
 import produce
 import sqlite3
 
+categories = ["Beverages", "wine-beer-spirits"]
 def getData():
-    url = "https://www.wholefoodsmarket.com/api/products/category/all-products?store=10245&limit=60&offset=939"
-    response = requests.get(url).json()
-    response = response["results"]
-    return response
+    for food in categories:
+        x = 0
+        while x < 939: 
+            url = f"https://www.wholefoodsmarket.com/api/products/category/{food}?store=10245&limit=60&offset={x}"
+            response = requests.get(url).json()
+            response = response["results"]
+            if food == "Produce"  or food == "Meat" or food == "Seafood" or food == "Beverages":
+                populate_db(response, food)
+            elif food == "dairy-eggs":
+                populate_db(response, "Dairy & Eggs")
+            elif food == "pantry-essentials" or food == "breads-rolls-bakery" or food == "supplements" or food == "snacks-chips-salsas-dips":
+                populate_db(response, "Pantry")
+            elif food == "wine-beer-spirits":
+                populate_db(response, "Beverages")
+            x += 60
+        
 
-def populate_db():
-    data = getData()
+
+def populate_db(data, category):
     for x in data:
-        produce.insert_produce(x['name'],"https://www.wholefoodsmarket.com/product/" + x['slug'], x['imageThumbnail'], None , None,  x['regularPrice'], "Wholefoods", x['store'], None)
+        if 'imageThumbnail' in x:
+            produce.insert_produce(x['name'],"https://www.wholefoodsmarket.com/product/" + x['slug'], x['imageThumbnail'], None , None,  x['regularPrice'], "Wholefoods", x['store'], category)
+        else:
+            produce.insert_produce(x['name'],"https://www.wholefoodsmarket.com/product/" + x['slug'], None, None , None,  x['regularPrice'], "Wholefoods", x['store'], category)
 
 produce.create_produce_table()
-populate_db()
+getData()
 produce.display_produce()
 # print(getData())
 
